24baee72c2fc7a7a3df2117bfefd3fc98d3181a4
Ahmed AbdElnaby <ahmed.abd.elnaby@outlook.com>
2023-03-13 05:20:49 +0200

    adding license controller

---
+++ o-stock-microservices-app/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -0,0 +1,7 @@
+package com.optimagrowth.license.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/v1/organization/{organizationId}/license")
+public class LicenseController {
+}
