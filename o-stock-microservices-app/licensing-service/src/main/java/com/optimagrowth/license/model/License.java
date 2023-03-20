665443881aab541dbf694e7c3adef6fd3d95ade6
Ahmed AbdElnaby <ahmed.abd.elnaby@outlook.com>
2023-03-20 04:28:36 +0200

    Refactoring License model

--- o-stock-microservices-app/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ o-stock-microservices-app/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -1,17 +1,35 @@
 package com.optimagrowth.license.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.hateoas.RepresentationModel;
 
 @Getter @Setter @ToString
+@Entity // tells Spring this POJO will hold data
+@Table(name = "licenses") // tells Spring JPA what table this POJO will map to
 public class License extends RepresentationModel<License> { // for adding HATEOAS
 
     private int id;
+    @Id
+    @Column(name = "license_id", nullable = false)
     private String licenseId;
     private String description;
+    @Column(name = "organization_id", nullable = false)
     private String organizationId;
+    @Column(name = "product_name", nullable = false)
     private String productName;
+    @Column(name = "license_type", nullable = false)
     private String licenseType;
+    @Column(name = "comment")
+    private String comment;
+
+    public License withComment(String comment) {
+        this.setComment(comment);
+        return this;
+    }
 }
